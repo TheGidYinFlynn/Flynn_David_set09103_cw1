b47e8399474414689bb755201fab8f5e83634a1a
TheGidYinFlynn <TheGidYinFlynn@hotmail.co.uk>
2018-10-18 17:50:14 +0100

    added selection bar

--- coursework.py
+++ coursework.py
@@ -1,13 +1,26 @@
 from flask import Flask , render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/home/')
+def Home():
 	return render_template('home.html')
 
-@app.route('/goodbye/')
-def goodbye():
-    return 'goodbye everyone'
+@app.route('/character/')
+def Character():
+    return render_template('character.html')
+
+@app.route('/item')
+def item () :
+   return render_template('item.html')
+
+@app.route('/location')
+def location () :
+   return render_template('location.html')
+
+@app.route('/about')
+def about () : 
+   return render_template('about.html')
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=true)
